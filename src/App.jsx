c2c96050d5d9ade7d989f558a198e8fc0fa5e7ea
baltonCome <baltoncome@gmail.com>
2022-01-17 22:57:27 +0200

    Repo innit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+
+    return (
+        <>
+            <div className="container">
+                <h1>Hello Boys</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
