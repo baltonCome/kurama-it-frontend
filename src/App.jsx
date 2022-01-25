fd4740e7c83f27d21e49330cc7ce48af90b6121a
baltonCome <baltoncome@gmail.com>
2022-01-25 13:56:40 +0200

    cors done and retrieving data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import axios from 'axios';
 
 function App(){
 
-    return (
-        <>
-            <div className="container">
-                <h1>Hello Boys</h1>
+    const [jobs, setJobs] = useState([]);
 
-                <Link to="/Home">Home</Link>
-                <Link to="/Login">Login</Link>
-            </div>
+    axios.get('http://localhost:8000/sanctum/csrf-cookie', { withCredentials: true, headers : { 'Content-Type' : 'application/json' } });
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/job/java", { withCredentials : true }).then(({data}) => {
+            setJobs(data);
+        })    
+        console.log(jobs);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+       
+    return ( 
+        <>
+            <Container>
+                <h1>Hello Boys</h1>
+                <Link to="/home">Home</Link>
+                <p></p>
+                <Link to="/login">Login</Link>
+                <Outlet />
+            </Container>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./components/auth/Login";
 import Home from "./pages/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+axios.get('http://localhost:8000/sanctum/csrf-cookie', { withCredentials: true });
+axios.get('http://localhost:8000/api/');
+
+function getJobs(){
+  axios({
+    method: 'get',
+    url: 'http://localhost:8000/api/'
+  }).then(res => console.log(res)).catch(err => console.error(err));
+}
 
 const rootElement = document.getElementById("root");
 
@@ -16,3 +28,5 @@ render(
   </Router>,
   rootElement
 );
+
+getJobs();
